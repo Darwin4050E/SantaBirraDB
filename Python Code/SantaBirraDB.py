a2b94712ab03197f4e2398c8f8b5a9a99193962d
Gatumbac <gatumbac@espol.edu.ec>
2025-01-04 21:26:48 -0500

    Nueva funcionalidad inventario

--- Python Code/SantaBirraDB.py
+++ Python Code/SantaBirraDB.py
@@ -4,6 +4,7 @@ import mysql.connector
 from ventas import *
 from clientes import *
 from productos import *
+from inventario import *
 
 # --- Menú Principal ---
 def menu_principal(db):
@@ -13,7 +14,7 @@ def menu_principal(db):
         == Seleccione una opción ==
         1. Gestión de Clientes
         2. Gestión de Productos
-        3. Gestión de Reservas
+        3. Gestión de Inventarios
         4. Gestión de Ventas
         5. Generar Reportes
         6. Salir
@@ -26,7 +27,7 @@ def menu_principal(db):
         elif opcion == "2":
             menu_crud_productos(db)
         elif opcion == "3":
-            menu_crud_reservas(db)
+            menu_crud_inventario(db)
         elif opcion == "4":
             menu_crud_ventas(db)
         elif opcion == "5":
